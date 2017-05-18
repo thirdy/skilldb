e2522163f5bd5735c6e2ef1ff3abc3ac0ce14612
Vicente De Rivera <vderivera@corp.nextdigital.com>
2017-05-18 11:40:15 +0800

    added slf4j and log4j logging

---
+++ src/main/java/com/apd/skilldb/controller/ImportController.java
@@ -0,0 +1,43 @@
+package com.apd.skilldb.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import com.apd.skilldb.entity.Check;
+import com.apd.skilldb.service.CheckService;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Getter
+@Setter
+@ManagedBean
+@SessionScoped
+public class ImportController implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@ManagedProperty("#{checkService}")
+	private CheckService checkService;
+
+	private UploadedFile file;
+
+	public void handleFileUpload(FileUploadEvent event) {
+		UploadedFile file = event.getFile();
+		logger.info("File: " + file.getFileName());
+	}
+}
